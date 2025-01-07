b0159bbbd5a8bdf74d4db36a149c98617516fa2b
D-A-N-1-3-6 <158775247+D-A-N-1-3-6@users.noreply.github.com>
2025-01-07 12:30:57 +0100

    nueron impl.

---
+++ neuron.py
@@ -0,0 +1,15 @@
+import math
+
+# Model of a neuron
+
+def neuron(features: list[list[float]], labels: list[int], weights: list[float], bias: float) -> (list[float], float):
+    probabilities = []
+    for feature_vector in features:
+        z = sum(weight * feature for weight, feature in zip(weights, feature_vector)) + bias
+        probs = round(1 / (1 + math.exp(-z)), 4)
+        probabilities.append(probs)
+    
+    mse = round(sum((prob - label) ** 2 for prob, label in zip(probabilities, labels)) / len(labels), 4)
+    return probabilities, mse
+
+
